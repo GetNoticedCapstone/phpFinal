2aa221b26b03932aab915846e100f9818ff70506
David Landi <David Landi@DavidLandi-PC.home>
2014-12-13 16:44:12 -0500

    completed about_page

--- about_manager/about_page.php
+++ about_manager/about_page.php
@@ -1,17 +1,46 @@
 <?php include '../view/header_1.php'; ?>
 <div class="container centerAll">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="trans_box center-block text-center img-responsive ">
-                <form action="." method="post" class="aligned">
-                <h3>about page</h3>
+    <div class="trans_box_about center-block text-center img-responsive ">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h2 class="text-center text-danger txtShadow">About FFN</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
                 <div id="googleMap"></div>
-                
-                    <a class="btn-lg btn-warning btn" href=".?action=home">home</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="center-block">
+                    <h3 class="txtShadow text-danger">David Landi</h3>                                       
+                    <p>
+                    <a href="https://github.com/davidlandi" target="_blank" title=""><img src="../img/git.png" alt="" title="gitHub" class="ghSize"></a>
+                    <a href="https://linkedin.com/pub/david-landi/51/838/b4b/" target="_blank" title=""><img src="../img/linkin.png" alt="" title="LinkedIn" class="liSize"></a>
+                    </p>  
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class=" center-block">
+                    <h3 class="txtShadow text-danger">Justin Barreira</h3>                                     
+                    <p>
+                     <a href="https://github.com/JustinBarreira" target="_blank" title=""><img src="../img/git.png" alt="" title="gitHub" class="ghSize"></a>
+                     <a href="#" target="_blank" title=""><img src="../img/linkin.png" alt="" title="LinkedIn" class="liSize"></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <form action="." method="post" class="aligned">
+                    <a class="btn-lg btn-danger btn aboutBtn" href=".?action=home">home</a>
                 </form>
             </div>
         </div>
-    </div>
+    </div>  
 </div>
 
 <?php
