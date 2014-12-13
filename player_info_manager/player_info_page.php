d7719f5a5904c91322b5627fc6190bcd5fe2474d
David Landi <David Landi@DavidLandi-PC.home>
2014-12-13 18:04:22 -0500

    completed player_info_page.php.  This page will act as a central hub to view different types of info.

--- player_info_manager/player_info_page.php
+++ player_info_manager/player_info_page.php
@@ -1,15 +1,31 @@
 <?php include '../view/header_1.php'; ?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="trans_box center-block text-center img-responsive centerAll">
-                <h3>player info</h3>
-                <form action="." method="post" class="aligned">
-                    <a class="btn-lg btn-success btn" href=".?action=stats">Player Stats</a>
-                    <a class="btn-lg btn-success btn" href=".?action=news">news</a>
-                    <a class="btn-lg btn-success btn" href=".?action=home">home</a>
-                </form>
-                
+<div class="container centerAll">
+    <div id="nflSchedule">
+        <div class="trans_box_news center-block img-responsive text-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2 class="text-center text-danger txtShadow">Information Hub</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class=" center-block imgSpace">
+                        <img src="../img/football.jpg" alt="">
+                    </div>
+                        
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <form action="." method="post" class="aligned">
+                    <a class="btn-lg btn-danger btn" href=".?action=stats">Player Stats</a>
+                    <a class="btn-lg btn-danger btn" href=".?action=news">NFL Schedule</a>
+                    <a class="btn-lg btn-danger btn" href=".?action=home">Home</a>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
