c7da7e5f69d99268a52cb2f494a8ca613131c333
David Landi <David Landi@DavidLandi-PC.home>
2014-12-13 13:09:16 -0500

    added doc strings

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php include 'view/header.php'; ?>
+<?php include 'view/header.php';
+/*This index.php which is in the root of the project is the signal access point 
+  for any users accessing our app.  the a href links are directing traffic 
+  to the controller which maintaines all of the logic and determines what the 
+  next step should be. 
+ */
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
